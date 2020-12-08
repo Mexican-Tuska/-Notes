a7a71b04d11f5e2a4b8f02273086d9df37e412b2
oshi <aaaaa>
2020-12-08 19:38:07 +0300

    First commit

---
+++ CreationWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Notes
+{
+    /// <summary>
+    /// Interaction logic for CreationWindow.xaml
+    /// </summary>
+    public partial class CreationWindow : Window
+    {
+        public CreationWindow()
+        {
+            InitializeComponent();
+        }
+        public CreationWindow(string title)
+        {
+            InitializeComponent();
+            this.set_view(title);
+        }
+        public event EventHandler CreationWindowClosed;
+        private void CreationWIndow_Closed(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //SaveFileDialog dlg = new SaveFileDialog();
+            //dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            //if (dlg.ShowDialog() == true)
+            //{
+            string title = Note_Title.Text.ToString();
+            string cur_dir = Directory.GetCurrentDirectory();
+                FileStream fileStream = new FileStream($"{title}.rtf", FileMode.Create);
+                TextRange range = new TextRange(NoteEditor.Document.ContentStart, NoteEditor.Document.ContentEnd);
+                range.Save(fileStream, DataFormats.Rtf);
+          
+                CreationWindowClosed(this, EventArgs.Empty);
+            //}
+        }
+
+        public string get_title()
+        {
+            return Note_Title.Text.ToString();
+        }
+      public void set_view(string title)
+        {   FileStream note_file = File.Open($"{title}.rtf", FileMode.Open);
+            Note_Title.Text = title;
+           TextRange doc = new TextRange(NoteEditor.Document.ContentStart, NoteEditor.Document.ContentEnd);
+           doc.Load(note_file, DataFormats.Rtf);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace Notes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<string> notes_titles;
+        public MainWindow()
+        {
+            InitializeComponent();
+            notes_titles = new List<string> ();
+
+        }
+        public string note_title;
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            CreationWindow creationWindow = new CreationWindow();
+            creationWindow.Owner = this;
+            creationWindow.CreationWindowClosed += CreationWindowClosed_React;
+            creationWindow.Show();
+                   
+        }
+
+        private void CreationWindowClosed_React(object sender, EventArgs e)
+        {
+            NotesList.Items.Add(((CreationWindow)sender).get_title());
+           
+        }
+
+        private void NotesList_ItemDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            int index = NotesList.SelectedIndex;
+            string tit = (string) NotesList.Items[index];
+            CreationWindow s_window = new CreationWindow(tit);
+            s_window.Owner = this;
+            
+            // s_window.set_view(tit);
+            s_window.Show();
+        }
+    }
+}
