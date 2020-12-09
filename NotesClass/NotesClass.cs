533b8c30de15340a275c496cf0582e79eb3677bf
oshi <aaaaa>
2020-12-09 20:03:33 +0300

    данные в классе

--- NotesClass/NotesClass.cs
+++ NotesClass/NotesClass.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace NotesClass
 {
     public class NotesClass
     {
+       
+        Dictionary<string, FileStream> notes;  
+        NotesClass ()
+        {
+            
+        }
+        NotesClass (int a)
+        {
+            notes = new Dictionary<string, FileStream>(a);
+        }
     }
 }
